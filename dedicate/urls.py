7eb8947fa519527308c9d8cb69443f99751e32aa
RiturajAgrahari <agr.rituraj@gmail.com>
2024-04-19 22:05:04 +0530

    executing view is added

--- dedicate/urls.py
+++ dedicate/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.home_page, name="home"),
     path("extract/", views.extract, name="extract"),
     path("ls/", views.list_directory, name="list_directory"),
+    path("enable-execution/", views.enable_execution, name="enable_execution"),
+    path("execute/", views.execute, name="execution"),
 ]
--- dedicate/views.py
+++ dedicate/views.py
@@ -23,3 +23,19 @@ def list_directory(request):
 
     return render(request, "home.html")
 
+
+def enable_execution(request):
+    command = 'chmod +x DedicateServers/server001/LinuxServer/LinuxDedicateTeot.x86_64'
+    output = subprocess.check_output(command, shell=True)
+    print(output.decode('utf-8'))
+
+    return render(request, "home.html")
+
+
+def execute(request):
+    command = 'nohup ./DedicateServers/server001/LinuxServer/LinuxDedicateTeot.x86_64 &'
+    output = subprocess.check_output(command, shell=True)
+    print(output.decode('utf-8'))
+
+    return render(request, "home.html")
+
